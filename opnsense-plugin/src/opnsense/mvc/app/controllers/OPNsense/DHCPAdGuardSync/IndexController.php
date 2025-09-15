1661a1fd363f91db87c8487e8bfd63764e37ebaf
Jeef <jeffstein@gmail.com>
2025-09-15 04:42:17 -0600

    fix: update IndexController to use formSettings variable
    - Change variable name from generalForm to formSettings in IndexController
    - Fix blank General page issue by matching variable names between controller and template
    - Ensures form is properly displayed in the UI

--- opnsense-plugin/src/opnsense/mvc/app/controllers/OPNsense/DHCPAdGuardSync/IndexController.php
+++ opnsense-plugin/src/opnsense/mvc/app/controllers/OPNsense/DHCPAdGuardSync/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends \OPNsense\Base\IndexController
     public function indexAction()
     {
         // Include form definitions
-        $this->view->generalForm = $this->getForm("general");
+        $this->view->formSettings = $this->getForm("general");
 
         // Pick the template to serve
         $this->view->pick('OPNsense/DHCPAdGuardSync/index');
